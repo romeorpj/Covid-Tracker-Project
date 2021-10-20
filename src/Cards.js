86f32be3da5478bf557518e00f3501aa5659b41e
richardj-codes <74265663+richardj-codes@users.noreply.github.com>
2021-10-20 11:21:21 -0500

    pushing current work

---
+++ src/Cards.js
@@ -0,0 +1,37 @@
+import React from "react";
+import numeral from "numeral";
+
+function Cards(props) {
+	// prettyNum uses the numeral package to format numbers
+	const prettyNum = (stat) =>
+		stat ? `+${numeral(stat).format("0.0a")}` : "+0";
+
+	return (
+		<div className="cards">
+			<div className="cards__info">
+				<p className="cards__headline">
+					Total Cases in {props.yourState.state}
+				</p>
+				<p className="cards__data">{prettyNum(props.yourState.cases)}</p>
+			</div>
+			<div className="cards__info">
+				<p className="cards__headline">Recovered: </p>
+				<p className="cards__data-green">
+					{prettyNum(props.yourState.recovered)}
+				</p>
+			</div>
+			<div className="cards__info">
+				<p className="cards__headline">Deaths: </p>
+				<p className="cards__data">{prettyNum(props.yourState.deaths)}</p>
+			</div>
+			<div className="cards__info">
+				<p className="cards__headline">Currently Infected: </p>
+				<p className="cards__data">{prettyNum(props.yourState.active)}</p>
+
+				<br />
+			</div>
+		</div>
+	);
+}
+
+export default Cards;
---
+++ src/CovidDashboard.js
@@ -0,0 +1,12 @@
+import SearchInput from "./SearchInput";
+
+const CovidDashboard = () => {
+	return (
+		<section className="dashboard-wrapper">
+			<div className="dashboard-elements">
+				<SearchInput />
+			</div>
+		</section>
+	);
+};
+export default CovidDashboard;
